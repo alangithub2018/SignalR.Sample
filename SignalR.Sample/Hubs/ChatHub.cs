472be2cda962fc96ede85b88fe32aa36ca9bda91
Eric Alan Rodriguez Contreras <licalan.sistemas@gmail.com>
2025-02-01 21:20:44 -0600

    Getting started with simple chat implementation

---
+++ SignalR.Sample/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Sample.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageToAll(string user, string message)
+        {
+            await Clients.All.SendAsync("MessageReceived", user, message);
+        }
+    }
+}
---
+++ SignalR.Sample/wwwroot/js/chat.js
@@ -0,0 +1,24 @@
+﻿var chatConnection = new signalR.HubConnectionBuilder().withUrl("/hubs/chat").build();
+
+document.getElementById("sendMessage").disabled = true;
+
+chatConnection.on("MessageReceived", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user} - ${message}`;
+});
+
+document.getElementById("sendMessage").addEventListener("click", function (event) {
+    var sender = document.getElementById("senderEmail").value;
+    var message = document.getElementById("chatMessage").value;
+
+    chatConnection.send("SendMessageToAll", sender, message).catch(function (error) {
+        return console.error(error.toString());
+    });
+
+    event.preventDefault();
+});
+
+chatConnection.start().then(function () {
+    document.getElementById("sendMessage").disabled = false;
+});
